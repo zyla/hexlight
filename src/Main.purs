ea2ceaca19bd6ff7025c2d990f74678fd2f31a77
Hannes Rantzsch <gh@hannesrantzsch.de>
2018-01-11 22:21:55 +0100

    an event that doesn't do anything

--- src/Main.purs
+++ src/Main.purs
@@ -6,32 +6,44 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import DOM (DOM)
 import Data.Traversable (for_)
+import Mock (demoText)
 import Pux (EffModel, noEffects, start)
+import Pux.DOM.Events (DOMEvent, onChange, onClick, onPaste, targetValue)
 import Pux.DOM.HTML (HTML)
-import Pux.DOM.Events (onPaste)
 import Pux.Renderer.React (renderToDOM)
 import Signal.Channel (CHANNEL)
-import Text.Smolder.HTML (div, h2, textarea)
+import Text.Smolder.HTML (button, div, h2, textarea)
 import Text.Smolder.Markup ((#!), text)
 
-data Event = Hello | World
+data Event = InputChange DOMEvent
 
-type State = { colors :: Array String  }
+type Color = String
+type State =
+  { input :: String
+  , colors :: Array Color
+  }
+
+parseInput :: String -> Array Color
+parseInput i = []
+
 init :: State
-init = { colors: ["#f00f00", "#001337"] }
+init = { input: "", colors: [] }
 
 foldp :: ∀ fx. Event -> State -> EffModel State Event (dom :: DOM | fx)
-foldp _ state = noEffects $ state
+foldp (InputChange event) state = noEffects $ state { input = targetValue event }
 
 view :: State -> HTML Event
 view state = div do
   h2 $ text "paste text and code here"
-  textarea #! onPaste (const Hello) $ text ""
-  h2 $ text "colors found"
+  textarea $ text demoText #! onPaste InputChange #! onChange InputChange
+  -- button #! onClick (const Hello) $ text "eval"
+  h2 $ text "colors found:"
   for_ state.colors \c -> do
     text c
+  h2 $ text "input:"
+  text state.input
 
-main :: forall fx. Eff (channel :: CHANNEL, exception :: EXCEPTION, dom :: DOM | fx) Unit
+main :: ∀ fx. Eff (channel :: CHANNEL, exception :: EXCEPTION, dom :: DOM | fx) Unit
 main = do
   app <- start
     { initialState: init
---
+++ src/Mock.purs
@@ -0,0 +1,11 @@
+module Mock where
+
+demoText :: String
+demoText =
+  "# Theme colors (navy-and-ivory - terminal.sexy) \
+  \# class                 border  backgr. text    indic.    child_border \
+  \# client.focused          #9ED9D8 #44B5B1 #FFFFFF #FF5879   #9ED9D8 \
+  \# client.focused_inactive #2E3340 #61778D #E8DFD6 #2E3340   #61778D \
+  \# client.unfocused        #2E3340 #2E3340 #E8DFD6 #2E3340   #61778D \
+  \# client.urgent           #C2454E #C2454E #E8DFD6 #C2454E   #C2454E \
+  \# client.placeholder      #000000 #0C0C0C #E8DFD6 #000000   #0C0C0C"

140cb3ec459ddfe5fab670df2a7d09f177ab41f0
Hannes Rantzsch <gh@hannesrantzsch.de>
2018-01-13 01:13:28 +0100

    - work in progress -

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,24 @@ module Main where
 
 import Prelude hiding (div)
 
+import Control.Monad.Aff.Console (log) as Aff
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM (DOM)
-import Data.Traversable (for_)
+import Data.Either (Either(..))
+import Data.Foldable (for_, sequence_, traverse_)
+import Data.Maybe (Maybe(..))
+import Data.String.Regex (match, regex)
+import Data.String.Regex.Flags (global)
+import Data.Traversable (sequenceDefault, traverseDefault)
 import Mock (demoText)
-import Pux (EffModel, noEffects, start)
-import Pux.DOM.Events (DOMEvent, onChange, onClick, onPaste, targetValue)
+import Prelude (join)
+import Pux (EffModel, CoreEffects, start)
+import Pux.DOM.Events (DOMEvent, onChange, targetValue)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (renderToDOM)
-import Signal.Channel (CHANNEL)
-import Text.Smolder.HTML (button, div, h2, textarea)
-import Text.Smolder.Markup ((#!), text)
+import Text.Smolder.HTML (div, h2, textarea)
+import Text.Smolder.Markup (text, (#!))
 
 data Event = InputChange DOMEvent
 
@@ -23,28 +29,42 @@ type State =
   , colors :: Array Color
   }
 
-parseInput :: String -> Array Color
-parseInput i = []
+matchInput :: String -> Maybe (Array (Maybe Color))
+-- matchInput input = case regex "#[[:xdigit:]]+" global of
+matchInput input = case regex "#[0-9a-fA-F]+" global of
+  Right rx -> match rx input
+  Left _ -> Nothing
+
+-- r :: Array (Maybe Color) -> Maybe (HTML Event)
+-- r c = traverse_ text <$> sequenceDefault c
+
+renderColors :: Maybe (Array (Maybe Color)) -> HTML Event
+renderColors Nothing = text "no match"
+renderColors (Just colorMatches) =
+  case sequenceDefault colorMatches of
+    Nothing      -> text "no match"
+    Just matches -> for_ matches \color -> do
+      text color
 
 init :: State
 init = { input: "", colors: [] }
 
-foldp :: ∀ fx. Event -> State -> EffModel State Event (dom :: DOM | fx)
-foldp (InputChange event) state = noEffects $ state { input = targetValue event }
+foldp :: Event -> State -> EffModel State Event (console :: CONSOLE, dom :: DOM)
+foldp (InputChange ev) s=
+  { state: s { input = targetValue ev }
+  , effects: [ Aff.log "update state" *> pure Nothing ]
+  }
 
 view :: State -> HTML Event
 view state = div do
   h2 $ text "paste text and code here"
-  textarea $ text demoText #! onPaste InputChange #! onChange InputChange
-  -- button #! onClick (const Hello) $ text "eval"
+  textarea #! onChange InputChange $ text demoText
   h2 $ text "colors found:"
-  for_ state.colors \c -> do
-    text c
-  h2 $ text "input:"
-  text state.input
+  renderColors $ matchInput state.input
 
-main :: ∀ fx. Eff (channel :: CHANNEL, exception :: EXCEPTION, dom :: DOM | fx) Unit
+main ::  Eff (CoreEffects (console :: CONSOLE, dom :: DOM)) Unit
 main = do
+  log "starting app"
   app <- start
     { initialState: init
     , view
@@ -52,4 +72,5 @@ main = do
     , inputs: []
     }
 
+  log "rendering"
   renderToDOM "#app" app.markup app.input

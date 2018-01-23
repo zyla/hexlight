ca63ed6f1ae6c513949ad76bab1b5d08568e50a9
Hannes Rantzsch <gh@hannesrantzsch.de>
2018-01-23 23:19:21 +0100

    better level of abstraction for showColors

--- src/Main.purs
+++ src/Main.purs
@@ -2,16 +2,14 @@ module Main where
 
 import Prelude hiding (div)
 
-import Control.Monad.Aff.Console (log) as Aff
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 import DOM (DOM)
 import Data.Either (Either(..))
-import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
 import Data.String.Regex (match, regex)
 import Data.String.Regex.Flags (global)
-import Data.Traversable (sequenceDefault)
+import Data.Traversable (for_, sequence, traverse)
 import Mock (demoText)
 import Pux (EffModel, CoreEffects, start)
 import Pux.DOM.Events (DOMEvent, onChange, targetValue)
@@ -35,17 +33,11 @@ matchInput input = case regex "#[0-9a-fA-F]+" global of
   Right rx -> match rx input
   Left _ -> Nothing
 
--- r :: Array (Maybe Color) -> Maybe (HTML Event)
--- r c = traverse_ text <$> sequenceDefault c
-
--- TODO: fmap or bind the 'no match' one up
--- TODO: provide default value for 'Nothing' case (removing all HTML here)
-showColors :: Maybe (Array (Maybe Color)) -> (Color -> HTML Event) -> HTML Event
-showColors Nothing show = show "no match"
-showColors (Just colorMatches) show =
-  case sequenceDefault colorMatches of
-    Nothing      -> show "no match"
-    Just matches -> for_ matches show
+showColors :: Maybe (Array Color) -> HTML Event
+showColors Nothing = div ! Attr.className "pure-u-1-8" $ span $ text "none"
+showColors (Just colors) = for_ colors \color -> do
+  div ! Attr.className "pure-u-1-8" $ do
+    span ! Attr.style ("background-color: " <> color) $ text color
 
 init :: State
 init = { input: "", colors: [] }
@@ -60,16 +52,16 @@ view :: State -> HTML Event
 view state = div ! Attr.className "layout" $ do
   -- TODO CSS: make it all less ugly
   h2 $ text "paste text and code here"
-  div do
-    textarea #! onChange InputChange ! Attr.className "pure-input-1" $ text demoText
+  div $ textarea #! onChange InputChange ! Attr.className "pure-input-1"
+    $ text demoText
   h2 $ text "colors found:"
+
   -- TODO: fg color white or black depending on bg
   --   sa https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color#3943023
-  let showColor c = div ! Attr.className "pure-u-1-8" $ do
-                      span ! Attr.style ("background-color: " <> c) $ text c
   -- TODO CSS: make grid flow
+
   div ! Attr.className "pure-g" $ do
-    showColors (matchInput state.input) showColor
+    showColors $ traverse join $ sequence (matchInput state.input)
 
 main ::  Eff (CoreEffects (console :: CONSOLE, dom :: DOM)) Unit
 main = do
